e706069a3c3d658cd57e9c2c280672444d65220d
liyq <2500464546@qq.com>
2019-05-31 17:12:23 +0800

    update

---
+++ src/router.js
@@ -0,0 +1,43 @@
+import Vue from "vue";
+import Router from "vue-router";
+import Home from "./views/Home.vue";
+
+Vue.use(Router);
+
+export default new Router({
+  mode: "history",
+  base: process.env.BASE_URL,
+  routes: [
+    {
+      path: "/user",
+      component: {
+        render: h => h("router-view")
+      },
+      children: [
+        {
+          path: "/user/login",
+          name: "login",
+          component: () =>
+            import(/* webpackChunkName: "user" */ "./views/User/Login")
+        },
+        {
+          path: "/user/register",
+          name: "register",
+          component: () =>
+            import(/* webpackChunkName: "user" */ "./views/User/Register")
+        }
+      ]
+    },
+    {
+      path: "/",
+      name: "home",
+      component: Home
+    },
+    {
+      path: "/about",
+      name: "about",
+      component: () =>
+        import(/* webpackChunkName: "about" */ "./views/About.vue")
+    }
+  ]
+});

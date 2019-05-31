c491be0b4152cf09e1f1b80db195a8eabb9c8301
liyq <2500464546@qq.com>
2019-05-31 15:12:56 +0800

    delete

--- src/views/User/Login.vue
+++
@@ -1,5 +0,0 @@
-<template>
-  <div>
-    <h1>login页面出差</h1>
-  </div>
-</template>
--- src/views/User/Register.vue
+++
@@ -1,9 +0,0 @@
-<template>
-  <div>注册页面</div>
-</template>
-
-<script>
-export default {};
-</script>
-
-<style></style>
